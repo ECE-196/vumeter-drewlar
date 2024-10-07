1fc0e253011fc1f9a22e5796aa83c70d04f8c594
Andrew Lara <130115948+drewlar@users.noreply.github.com>
2024-10-06 17:27:35 -0700

    Completed Code and Extra Credit

--- main.py
+++ main.py
@@ -4,16 +4,24 @@ from analogio import AnalogIn
 from time import sleep
 
 # setup pins
-microphone = AnalogIn(board.IO1)
+microphone = AnalogIn(board.IO1) # Microphone is set up on SIG / IO1
 
-status = DigitalInOut(board.IO17)
+status = DigitalInOut(board.IO17) # ??
 status.direction = Direction.OUTPUT
 
 led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33,
+    board.IO34,
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
+   
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -24,14 +32,9 @@ for led in leds:
 # main loop
 while True:
     volume = microphone.value
-
-    print(volume)
-
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
-
-    sleep(1)
-
-    # instead of blinking,
-    # how can you make the LEDs
-    # turn on like a volume meter?
+    leds[0].value = volume >= 23000 # Lowest level is independent
+    for i in range(1,10):
+        sleep(.005 * (9-i)) # The lower the volume the longer it takes to change
+        leds[i].value = (volume >= 23000 + (500 * i)
+                          or leds[i+1].value) # LED cannot turn off if the one above it is on
+    leds[10].value = volume >= 27500 # Highest level is independent
